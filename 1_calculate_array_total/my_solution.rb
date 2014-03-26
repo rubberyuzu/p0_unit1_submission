9055aed30973ecf07e3e5786bb5568f95ffd6721
rubberyuzu <yuzu0125@gmail.com>
2014-03-25 18:47:41 -0700

    Update my_solution.rb

--- 1_calculate_array_total/my_solution.rb
+++ 1_calculate_array_total/my_solution.rb
@@ -4,22 +4,57 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge with: Andra.
 
-# 1. Pseudocode
-
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
+//Pseudo Code
+//Make method that adds the sums of different length of array.
+//Name the method "total".
 
 
-# 2. Initial Solution
+def total(array)
+ sum = array.inject do |a,b| a + b 
+ end
+end
+
+puts([])
+
+
+//refactor
+
+def total(array)
+ sum = array.inject {|a,b| a + b} 
+ end
+
+puts total([])
 
 
 
+//Pseudo Code
+//Make a method that takes a array parameter and return a sentence. 
+//Name the method sentence_maker
 
-# 3. Refactored Solution
+
+def sentence_maker(array)
+    sentence = array.join(" ")
+    (sentence + ".").capitalize
+end
+
+puts sentence_maker(["i","like","pizza"])
 
 
+//refactor
 
-# 4. Reflection 
+def sentence_maker(array)
+    (array.join(" ") + (".")).capitalize
+end
+
+puts sentence_maker(["i","like","pizza"])
+
+
+# reflection
+# I got back the feeling of writing in Ruby!
+# In this challenge I got familiar with inject method and join method.
+# Overall I had really much fun working with Andra! She knew something I did not know and so did I.
+# I think we did pretty much a good collaboration.
+
+
