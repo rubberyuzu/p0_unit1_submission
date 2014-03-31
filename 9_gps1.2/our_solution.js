5bf60c450259790b9c76e5704c7092cc67aecfa3
rubberyuzu <yuzu0125@gmail.com>
2014-03-30 20:28:16 -0700

    updated gps1.2

--- 9_gps1.2/our_solution.js
+++ 9_gps1.2/our_solution.js
@@ -6,17 +6,45 @@
 
 
 // 1. "YOU SIGNED... WHO?!"
+// create an object for new client using object initializer.
+// write your code here. 
 
+adam = {
+  name:"Adam Sandler", 
+  age:47, 
+  quote:"That's your home! Are you too good for your home?!"};
 
+kristen={
+    name:"Kristen Bell",
+    age:33,
+    quote: "Do you wanna build a snowman?"
+};
 
+jim={
+    name:"Jim Carrey",
+    age:52,
+    quote:"...So you're telling me there's a chance? YEAH!"
+};
 
 // 2. "Here they Come!"
 
-
-
-
-
 // 3. "TIME IS MONEY!"
+// make a function constructor called "client"
+// add properties (name, age, and quote)
+// make instances adam, kristen and jim
+
+function Client(name, age, quote){
+    this.name = name;
+    this.age = age;
+    this.quote = quote;
+}
+
+var adam = new Client("Adam Sandler",47,"That's your home! Are you too good for your home?!")
+var kristen = new Client("Kristen Bell", 33, "Do you wanna build a snowman?" )
+var jim= new Client("Jim Carrey", 52, "...So you're telling me there's a chance? YEAH!")
+
+
+
 
 //YOUR CODE HERE!
 
@@ -26,12 +54,6 @@ shooterMcGavin.age === 48;
 shooterMcGavin.quote === "Just stay out of my way... or you'll pay. Listen to what I say.";
 
 
-
-
-
 // 4. "SHOW 'EM OFF!"
+return ""+""
 
-
-
-
-// ** BONUS **
